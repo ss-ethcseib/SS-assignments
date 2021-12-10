771b338f472e34a1e10bab7cadf3ba5478f47338
SkinlessMilk3 <ethancoreyseiber@hotmail.com>
2021-12-10 10:23:13 -0600

    Added profiling to assignment 2 and provided answers for assignment 4.

---
+++ Week1/Day4/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <assert.h>
+
+using namespace std;
+
+int main(void) { assert(3 == 3.0); float x = 3.1; assert(x == 3.1f); assert(3.1 == 3.1); assert(true == 1.0); assert('a' == 97); int* p = 0; assert(NULL == p); assert(NULL == 0); }
