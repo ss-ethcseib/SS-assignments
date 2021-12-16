8f0717a5956cacc097d5e4c00eeae5c269ad7a8e
ss-ethcseib <95649940+ss-ethcseib@users.noreply.github.com>
2021-12-16 12:31:42 -0600

    updated and cleaned up rvalue_references.cpp

--- Week2/Day4/Assign1/src/rvalue_references.cpp
+++ Week2/Day4/Assign1/src/rvalue_references.cpp
@@ -4,11 +4,11 @@
 
 class Grade {
 	std::unordered_map<int, char> grades;//Don't know about this. Quite confusing in the explanation.
-public: 
+public:
 	Grade() {
 		grades = { {10, 'D'}, {20, 'C'}, {30, 'B'}, {40, 'A'} };
 	}
-	char grade(const int&& score){
+	char grade(const int&& score) {
 		return grades[score];
 	}
 };
@@ -16,8 +16,13 @@ int main(void) {
 	Grade g;
 	int arr[] = { 10, 30, 20, 20 };
 
+	for (int i = 0; i < 4; i++)
+		std::cout << "Number grades: " << arr[i] << std::endl;
+
+	std::cout << std::endl;
+
 	for (int i = 0; i < 4; i++) {
-		std::cout << "Grade: " << g.grade(std::move(arr[i])) << std::endl;
+		std::cout << "Letter grades: " << g.grade(std::move(arr[i])) << std::endl;
 	}
 
 	return 0;
