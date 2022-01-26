37d665392658f3d71a5cc122d02e9aea8a6455a4
SkinlessMilk3 <ethancoreyseiber@hotmail.com>
2022-01-26 07:58:07 -0600

    Finished Day25 stuff.

---
+++ Day25/Assign1/src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <future>
+#include <thread>
+#include <numeric>
+
+template<class T>
+void accumulate(std::vector<T>& vec, std::promise<T>& pr){
+  
+  pr.set_value(std::accumulate(vec.begin(), vec.end(), 0));
+}
+
+int main(){
+  std::promise<int> pr;
+  std::future<int> fut = pr.get_future();
+
+  std::vector<int> vec = {0, 1, 2, 3, 4, 5};
+
+  std::thread th(accumulate<int>, std::ref(vec), std::ref(pr));
+
+  std::cout << "Promise/future value should be: " << std::accumulate(vec.begin(), vec.end(), 0) << "\n\tvalue: " << fut.get() << std::endl;
+  
+  th.join();
+
+  return 0;
+}
